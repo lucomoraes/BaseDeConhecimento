d37c5437f6eff78b9ef5db64b33859d48afb6c07
Lucas <lcmoraes94@hotmail.com>
2021-04-26 11:02:13 -0300

    feito ajuste no script de dropdown estava faltando um ';'

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function visibilidadeTopico(id) {
     let elemento = document.getElementById(id);
-    
     if(elemento.style.display == "block") {
         elemento.style.display = "none"; 
     } else {
-        elemento.style.display = "block"
+        elemento.style.display = "block";
     }
 }
 
